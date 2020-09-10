14bfebd8caa00e3c05d81543638457c334d96059
Mr-Bakai <ismailov.time@gmail.com>
2020-09-10 22:13:38 +0600

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        ageAndTemp(20, 33);
-        ageAndTemp(20, 15);
-        ageAndTemp(1, 43);
-        ageAndTemp(50, 20);
-        ageAndTemp(30, 32);
-
+        System.out.println(ageAndTemp(20, 45));
+        System.out.println(ageAndTemp(10, 85));
+        System.out.println(ageAndTemp(34, 10));
+        System.out.println(ageAndTemp(28, 98));
+        System.out.println(ageAndTemp(10, 56));
 
 
     }
@@ -17,23 +16,19 @@ public class Main {
 
 
         if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
-            System.out.println("You can go for a walk");
+            return "You can go for a walk";
 
         } else if (age < 20 && temperature > 0 && temperature < 28) {
-//            System.out.println("You can go for a walk");
 
             return "You can go for a walk";
-        } else if (age > 45) {
-            if (temperature > -10 && temperature < 25) {
-                System.out.println("You can take a walk");
-            }
+
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
+            return "You can go for a walk";
         } else {
-            System.out.println("You gotta stay at home");
+
+
+            return "You gotta stay at home";
         }
-        
-        return result;
-
-
 
 
     }
